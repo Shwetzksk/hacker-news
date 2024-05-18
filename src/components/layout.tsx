f2eaafccafcb8537c7569f92116cf9ace54a96e4
Shwetaksk <shweta.kumari@zybisys.com>
2024-05-19 00:10:10 +0530

    [enhancement]: Add navlink in navbar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,20 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import * as routePaths from "@/router/route-paths";
+import { Link, Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <section>
       <nav className="w-full bg-fuchsia-950 text-white px-3 py-1.5">
-        <h2>HackerNews</h2>
+        <h2 className="w-fit text-white">
+          <Link
+            to={routePaths.HOME}
+            className="w-fit text-white hover:text-current"
+          >
+            HackerNews
+          </Link>
+        </h2>
       </nav>
+
       <Outlet />
     </section>
   );
